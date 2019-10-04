4a2228f8f06488ce47350d6f1fcd18327f0f74e1
Marco Comes <comes95@gmail.com>
2019-10-04 11:56:13 +0200

    Change default database name

--- app/config.php
+++ app/config.php
@@ -4,14 +4,14 @@ return [
     'user' => [
         'host' => 'localhost',
         'port' => '5432',
-        'dbname' => 'food_express2',
+        'dbname' => 'food_express',
         'user' => 'role_utente',
         'password' => 'u123',
     ],
     'admin' => [
         'host' => 'localhost',
         'port' => '5432',
-        'dbname' => 'food_express2',
+        'dbname' => 'food_express',
         'user' => 'role_ristoratore',
         'password' => 'r123',
     ],
